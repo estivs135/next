09168cb95c446d3e4846108ea1da6afb36516d41
estivs135 <estevan.santos@gmail.com>
2024-07-11 15:29:14 -0300

    produtos grid responsive

--- loja/src/app/page.tsx
+++ loja/src/app/page.tsx
@@ -1,10 +1,24 @@
-import Image from "next/image";
+
 
 export default function Home() {
   return (
   
-    <div>
-      <h1>Next</h1>
+    <div className="max-w-7xl mx-auto pt-8 px-8 xl:px-0:">
+      <div
+      className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10 xl:gap-6">
+        <div className="bg-white rounded-lg shadow-lg overflow-hidden">
+            Prod
+        </div>
+        <div className="bg-white rounded-lg shadow-lg overflow-hidden">
+            Prod
+        </div>
+        <div className="bg-white rounded-lg shadow-lg overflow-hidden">
+            Prod
+        </div>
+        <div className="bg-white rounded-lg shadow-lg overflow-hidden">
+            Prod
+        </div>
+      </div>
     </div>
    
   );
