9abb0046bea17c5863cc9363ea86ea70fc5befca
estivs135 <estevan.santos@gmail.com>
2024-07-11 16:04:06 -0300

    typeproduct

--- loja/src/app/page.tsx
+++ loja/src/app/page.tsx
@@ -1,23 +1,28 @@
+import { ProductType } from "@/types/ProductType"
 
 
-export default function Home() {
+async function getProducts(){
+  const res = await fetch('https://fakestoreapi.com/products')
+
+  if(!res.ok){
+    throw new Error('faile to fetch')
+  }
+  return res.json()
+
+}
+export default async function Home() {
+  const products = await getProducts()
+  console.log(products)
   return (
   
     <div className="max-w-7xl mx-auto pt-8 px-8 xl:px-0:">
       <div
       className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10 xl:gap-6">
-        <div className="bg-white rounded-lg shadow-lg overflow-hidden">
-            Prod
-        </div>
-        <div className="bg-white rounded-lg shadow-lg overflow-hidden">
-            Prod
-        </div>
-        <div className="bg-white rounded-lg shadow-lg overflow-hidden">
-            Prod
-        </div>
-        <div className="bg-white rounded-lg shadow-lg overflow-hidden">
-            Prod
-        </div>
+        {products.map((product: ProductType) => (
+          <div key={product.id}>
+              {product.title}
+          </div>
+        ))}
       </div>
     </div>
    
